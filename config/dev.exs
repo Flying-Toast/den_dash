7827e8b1e7fc842a8a1c715e80d9c1c3fe4acd01
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-11-02 12:24:43 -0400

    add tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,8 @@ config :den_dash, DenDashWeb.Endpoint,
   secret_key_base: "FVxtThDUoYCL1iXiPfdPqLtndEojhiRZ5xE8qtmkwkGSD+IaEOQkFyRHg5FIfOxH",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
