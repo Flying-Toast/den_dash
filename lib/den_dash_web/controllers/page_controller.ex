ee42506ee2ea1a22c0956b81e14cfede5993e190
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-11-03 17:04:30 -0400

    separage templates for logged-in vs -out index

--- lib/den_dash_web/controllers/page_controller.ex
+++ lib/den_dash_web/controllers/page_controller.ex
@@ -2,7 +2,11 @@ defmodule DenDashWeb.PageController do
   use DenDashWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    if conn.assigns[:me] do
+      render(conn, "_logged_in_index.html")
+    else
+      render(conn, "_logged_out_index.html")
+    end
   end
 
   def support(conn, _params) do
