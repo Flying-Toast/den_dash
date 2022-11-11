cdde67f78efe9fa8bbf061b972f91af7d03515dc
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-11-11 12:48:12 -0500

    bye bye favicon

--- lib/den_dash_web/endpoint.ex
+++ lib/den_dash_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule DenDashWeb.Endpoint do
     at: "/",
     from: :den_dash,
     gzip: false,
-    only: ~w(assets fonts images favicon.png robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
